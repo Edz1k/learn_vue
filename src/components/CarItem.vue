20ee703ddaf9e384f50cf0620066cdc51a3b1b3a
Eduard <shartner2006@mail.ru>
2023-09-01 20:40:39 +0600

    Interesting Cards

--- src/components/CarItem.vue
+++ src/components/CarItem.vue
@@ -11,7 +11,20 @@
             ...props.car,
             price: props.car.price + ' RUB',
         }
-    })
+    });
+    const carRemakeSec = computed(()=>{
+        return{
+            ...props.car,
+            newpole: props.car.price * props.car.volume,
+        }
+    });
+    function changeColor(color) {
+        const crappyColors = ['#FF0000', '#00FF00', '#0000FF']
+        if(crappyColors.includes(color)){
+            return true;
+        }
+        return false;
+    }
 </script>
 
 <template>
@@ -22,11 +35,28 @@
       <p>Год выпуска: {{ props.car.year }}</p>
       <p>Объем двигателя: {{ props.car.volume }}</p>
       <p>Цвет: {{ props.car.color }}</p>
+      <p>Новое поле: {{ carRemakeSec.newpole }}</p>
+      <div class="chipes">
+        <span v-if="Number(carRemake.price.slice(0,-3)) > 1000000">Дорогой автомобиль</span>
+        <span v-if="changeColor(carRemakeSec.color)">Конченный цвет</span>
+      </div>
     </div>
   </template>
 
 <style scoped>
     .car img {
         width: 10%;
+    }
+    .chipes {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: column;
+    }
+    .chipes span{
+        margin: 5px;
+        padding: 5px;
+        border: 1px solid black;
+        border-radius: 5px;
     }
 </style>
