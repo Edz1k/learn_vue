4f22a50c151e616e7391d89c8decf017335c0754
Eduard <shartner2006@mail.ru>
2023-09-04 19:13:51 +0600

    01.09 classwork

--- src/components/CarItem.vue
+++ src/components/CarItem.vue
@@ -1,5 +1,8 @@
 <script setup>
     import { computed, defineProps } from 'vue';
+    import Card from 'primevue/card';
+    import Chip from 'primevue/chip';
+
     const props = defineProps({
         car: {
             type: Object,
@@ -10,17 +13,12 @@
         return{
             ...props.car,
             price: props.car.price + ' RUB',
-        }
-    });
-    const carRemakeSec = computed(()=>{
-        return{
-            ...props.car,
             newpole: props.car.price * props.car.volume,
         }
     });
     function changeColor(color) {
         const crappyColors = ['#FF0000', '#00FF00', '#0000FF']
-        if(crappyColors.includes(color)){
+        if(crappyColors.includes(color.toUpperCase())){
             return true;
         }
         return false;
@@ -28,24 +26,32 @@
 </script>
 
 <template>
-    <div class="car">
-      <img :src="carRemake.image" alt="car" />
-      <h2>{{ props.car.brand }}</h2>
-      <p>Цена: {{ carRemake.price }}</p>
-      <p>Год выпуска: {{ props.car.year }}</p>
-      <p>Объем двигателя: {{ props.car.volume }}</p>
-      <p>Цвет: {{ props.car.color }}</p>
-      <p>Новое поле: {{ carRemakeSec.newpole }}</p>
-      <div class="chipes">
-        <span v-if="Number(carRemake.price.slice(0,-3)) > 1000000">Дорогой автомобиль</span>
-        <span v-if="changeColor(carRemakeSec.color)">Конченный цвет</span>
-      </div>
-    </div>
+    <Card>
+      <template #header>
+        <img :src="carRemake.image" alt="car" class="car-image" />
+      </template>
+      <template #title> {{ carRemake.brand }} </template>
+      <template #content>
+        <p>Цена: {{ carRemake.price }}</p>
+        <p>Год выпуска: {{ carRemake.year }}</p>
+        <p>Объем двигателя: {{ carRemake.volume }}</p>
+        <p :style="`color: ${carRemake.color}`">Цвет: {{ carRemake.color }}</p>
+      </template>
+      <template #footer>
+        <div class="chipes">
+            <Chip v-if="Number(carRemake.price.slice(0, -3)) > 1000000" label="Дорогой" icon="pi pi-apple" />
+            <Chip v-else label="Дешевый" icon="pi pi-android" />
+            <Chip v-if="Number(carRemake.year) > 2010" label="Новый" icon="pi pi-briefcase" />
+            <Chip v-else label="Старый" icon="pi pi-briefcase" />
+          <span v-if="changeColor(carRemake.color)">Плохой цвет</span>
+        </div>
+      </template>
+    </Card>
   </template>
 
 <style scoped>
-    .car img {
-        width: 10%;
+    .car-image{
+        width: 100%;
     }
     .chipes {
         display: flex;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.mount('#app')
